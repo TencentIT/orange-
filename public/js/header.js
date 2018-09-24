f214f9bca83b62dd46aec6aa2f69c9b292114079
tencentit <1969368719@qq.com>
2018-09-24 23:24:42 +0800

    9.24商品详情页 product_details.html以及放大镜效果

--- public/js/header.js
+++ public/js/header.js
@@ -5,11 +5,29 @@ $(function(){
 		success:function(res){
 			//console.log("header",res);
 			$(res).replaceAll("#header");
-        var $search = $(".search_box .search");
+		var $search = $(".search_box .search"),
+			$input  = $(".search_box .search_input");
+			
         $search.click(function(){
-        location.href=
-          `http://localhost:3002/products.html?kw=${$input.val().trim()}`
-      })
+			// alert(1);
+			location.href=
+			`http://localhost:3002/product_details.html?kw=${$input.val().trim()}`;
+		})
+		
+		console.log($input);
+		//键盘回车 搜索 
+		$input.keyup(function(e){
+			if(e.keyCode == 13){
+				// alert(2222);
+				$search.click();
+			}
+		})
+
+		if(location.search.indexOf("kw=")!=-1){
+			var kw = location.search.split("=")[1];
+			$input.val(kw);
+		}
+
 		}
 	})
 })
