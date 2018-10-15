b596988c0168e5396fc5ff0c7670a2b313130297
TencentIT <1969368719@qq.com>
2018-10-15 22:00:08 +0800

    10.15 购物车全选按钮 有个坑注意

--- public/js/common.js
+++ public/js/common.js
@@ -10,7 +10,7 @@
 // 	}
 // 	return xhr;
 // }
-
+$(document).ready(function(){
   function createXhr(){
     var xhr=null;
     if(window.XMLHttpRequest){
@@ -23,9 +23,34 @@
 
  //全选
  $("#CheckedAll").click(function(){
+
+  console.log(this.checked)
+
    if(this.checked){
+    console.log(1,this.checked)
      $("input[type=checkbox][name=checkitems]").attr("checked",true)
    }else{
      $("input[type=checkbox][name=checkitems]").attr("checked",false)
    }
+
  })
+
+//  $("input[type=checkbox][name=checkitems]").click(function(){
+//    var flag = true;
+//    $("input[type=checkbox][name=checkitems]").each(function(){
+//      if(!this.checkded){
+//        flag = false
+//      }
+//      if(flag){
+//       $("#CheckedAll").attr("checked",true)
+//      }else{
+//       $("#CheckedAll").attr("checked",false)
+//      }
+//    })
+
+//  })
+
+
+
+})
+  
